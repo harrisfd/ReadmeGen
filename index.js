43c2eb26719e0528ea54a02c7bb88e57350d4c56
ldylicia <fharrisfoster@gmail.com>
2021-04-08 12:11:56 -0700

    added questions

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require("inquirer")
 const generate = require("./generate")
 
+const path = require("path")
+
+const fs = require("fs")
+
+
 function init() {
     inquirer.prompt([
         {
@@ -12,6 +17,31 @@ function init() {
             type: "input",
             name: "description",
             message: "What is the Description of the Project?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What is your deployed application link?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "provide description/instructions of your project"
+        },
+        {
+            type: "input",
+            name: "contributions",
+            message: "please add any necssary contributions. If none, just enter, no contributions at this time"
+        },
+        {
+            type: "input",
+            name: "tests/roadmap",
+            message: "What is the roadmap/test for your project?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "For Any Questions enter your email address"
         },
         {
             type: "confirm",
